f7b8027c9601e35640345973ec56d5182039b04d
alexbryw <alexbryw@gmail.com>
2020-02-18 10:39:22 +0100

    added an api to /userapi.
    It returns a json string of userlist.

--- Models/User.cs
+++ Models/User.cs
@@ -3,15 +3,19 @@ namespace AngularASPDotNet.Models
 {
     public class User
     {
-        readonly string Name;
-        readonly int UserId;
-
-        readonly DateTime LastLogin;
+        public readonly string Name;
+        public readonly int UserId;
+        public DateTime LastLogin {get; private set;}
 
         public User(string Name, int UserId, DateTime LastLogin){
             this.Name = Name;
             this.UserId = UserId;
             this.LastLogin = LastLogin;
         }
+
+        public void setLastLogin(DateTime newLastLogin){
+            LastLogin = newLastLogin;
+        }
+        
     }
 }
--- Models/UserList.cs
+++ Models/UserList.cs
@@ -4,13 +4,19 @@ namespace AngularASPDotNet.Models
 {
     public class UserList
     {
-        List<User> userList = new List<User>();
+        private List<User> ListOfUsers = new List<User>();
 
+        //The constructor adds users to ListOfUsers when it's instantiated.
         public UserList(){
-            userList.Add(new User("Alex", 1000, DateTime.Now));
-            userList.Add(new User("Lex", 1000, DateTime.Now));
-            Console.WriteLine(userList[0]);
-            Console.WriteLine(userList[1]);
+            var rnd = new Random();
+            ListOfUsers.Add(new User("Alex", rnd.Next(), DateTime.Now));
+            ListOfUsers.Add(new User("Lex", rnd.Next(), DateTime.Now));
+            ListOfUsers.Add(new User("John", rnd.Next(), DateTime.Now));
+            ListOfUsers.Add(new User("Pluto", rnd.Next(), DateTime.Now));
+        }
+
+        public List<User> getListOfUsers(){
+            return ListOfUsers;
         }
     }
 }
