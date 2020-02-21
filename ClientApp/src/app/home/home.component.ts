40c0ba1ef4f6f115ebebeb4f01bc28cc11ad6c25
alexbryw <alexbryw@gmail.com>
2020-02-21 10:37:08 +0100

    added api link and a baseUrl link

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public baseUrl: string;
+  public apiUrl: string
+  constructor(@Inject('BASE_URL') baseUrl: string){
+    this.baseUrl = baseUrl
+    this.apiUrl = baseUrl + "userapi"
+  }
 }
